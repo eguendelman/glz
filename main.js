6320ee7200794e60e49d6cf233b9544416d79d75
Eran Guendelman <eguser42@gmail.com>
2018-10-05 02:29:48 +0300

    load mp3 into current page rather than redirect

--- main.js
+++ main.js
@@ -1,9 +1,52 @@
-function getRedirectorURL()
+var PLAYBACK_RATE = 1.2;
+var MAX_ATTEMPTS = 3;
+
+function getRedirectorURL(now)
 {
-    let now = new Date();
-    let timeTag = strftime('%y%m%d-%H', now);
+    let timeTag = strftime("%y%m%d-%H", now);
     let url = `https://api.bynetcdn.com/Redirector/glz/${timeTag}_News/PD`;
     return url;
 }
 
-console.log(getRedirectorURL());
+function updateAudioElement(el, url)
+{
+    el.src = url;
+    el.playbackRate = PLAYBACK_RATE;
+    el.play();
+    el.style.display = "block";
+}
+
+function setAudioSource(el, callback) 
+{
+    let now = new Date();
+    let failedAttempts = 0;
+    let url = getRedirectorURL(now);
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(e) {
+        if (xhr.readyState == 4) {
+            if(xhr.status == 200)
+            {
+                console.log("redirect detected to: " + xhr.responseURL);
+                updateAudioElement(el, xhr.responseURL);
+                callback(true, now);
+            } else {
+                console.log("did not get valid redirect");
+                failedAttempts++;
+                if(failedAttempts >= MAX_ATTEMPTS) {
+                    console.log("giving up");
+                    callback(false, null);
+                } else {
+                    now.setHours(now.getHours() - 1);
+                    url = getRedirectorURL(now);
+                    console.log(`Trying ${url}`);
+                    xhr.open("GET", url, true);
+                    xhr.send();
+                }
+            }
+        }
+    }
+    console.log(`Trying ${url}`);
+    xhr.open("GET", url, true);
+    xhr.send();
+}
