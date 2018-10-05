585c84e8a31bcc80fd122f08046a629ef714d08e
Eran Guendelman <eguser42@gmail.com>
2018-10-05 10:49:06 +0300

    Fixed redirect location fetching using my own utility

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var PLAYBACK_RATE = 1.2;
 var MAX_ATTEMPTS = 3;
 
+// This is a utility I had to write in order to get the redirect location
+// of a page.  I had trouble using traditional (client-side) methods 
+// because their https url redirects to http
+var HEADER_UTIL_URL = "https://scripts.augmented.life/utils/get_http_header?url="
+
 function getRedirectorURL(now)
 {
     let timeTag = strftime("%y%m%d-%H", now);
@@ -16,39 +21,41 @@ function updateAudioElement(el, url)
     el.style.display = "block";
 }
 
-function setAudioSource(el, callback) 
+function findValidRedirect(now, failedAttempts, el, callback)
 {
-    let now = new Date();
-    let failedAttempts = 0;
     let url = getRedirectorURL(now);
-
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(e) {
-        console.log(xhr);
-        if (xhr.readyState == 4) {
-            if(xhr.status == 200)
+    let fullurl = HEADER_UTIL_URL + url;
+    console.log(fullurl);
+    fetch(fullurl)
+        .then(response => response.json())
+        .then(function(data) {
+            console.log(data);
+            if(data.status == "OK" && data.location != null && !data.location.includes("not_found"))
             {
-                console.log("redirect detected to: " + xhr.responseURL);
-                updateAudioElement(el, xhr.responseURL);
+                console.log(data.location);
+                let redirect_url = data.location;
+                redirect_url = redirect_url.replace("http:","https:");
+                console.log(redirect_url);
+                updateAudioElement(el, redirect_url);
                 callback(true, now);
-            } else {
+            }
+            else
+            {
                 console.log("did not get valid redirect");
-                console.log(xhr);
                 failedAttempts++;
                 if(failedAttempts >= MAX_ATTEMPTS) {
                     console.log("giving up");
                     callback(false, null);
                 } else {
                     now.setHours(now.getHours() - 1);
-                    url = getRedirectorURL(now);
-                    console.log(`Trying ${url}`);
-                    xhr.open("GET", url, true);
-                    xhr.send();
+                    findValidRedirect(now, failedAttempts, el, callback);
                 }
             }
-        }
-    }
-    console.log(`Trying ${url}`);
-    xhr.open("GET", url, true);
-    xhr.send();
+        });
+}
+
+function setAudioSource(el, callback) 
+{
+    let now = new Date();
+    findValidRedirect(now, 0, el, callback);
 }
